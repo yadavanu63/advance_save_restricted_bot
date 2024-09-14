a60035365ad1f9d94e43e74f73ce35e31cf1e330
ANU YADAV <163430575+yadavanu63@users.noreply.github.com>
2024-09-14 20:57:33 +0530

    Update pyroplug.py

--- ggn/assets/pyroplug.py
+++ ggn/assets/pyroplug.py
@@ -112,7 +112,7 @@ async def send_video_with_chat_id(client, sender, path, caption, duration, hi, w
             progress=progress_for_pyrogram,
             progress_args=(
                 client,
-                '**__Uploading by: [Rishav](https://t.me/forcesuscribe)__**\n ',
+                '**__Uploading by: [Tapori](https://t.me/taporibot_bot)__**\n ',
                 upm,
                 time.time()
             )
@@ -146,7 +146,7 @@ async def send_document_with_chat_id(client, sender, path, caption, thumb_path, 
             progress=progress_for_pyrogram,
             progress_args=(
                 client,
-                '**__Uploading:__**\n**__Bot made by [Rishav](https://t.me/forcesuscribe)__**',
+                '**__Uploading:__**\n**__Bot made by [Tapori](https://t.me/taporibot_bot)__**',
                 upm,
                 time.time()
             )
@@ -365,7 +365,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, file_n):
                 await client.edit_message_text(sender, edit_id, 'poll media cant be saved')
                 return 
             edit = await client.edit_message_text(sender, edit_id, "__**Trying to Download**...")
-            file = await userbot.download_media(msg, progress=progress_for_pyrogram, progress_args=(client, "**__Unrestricting__: __[RIZZ EDUCATION](https://t.me/forcesuscribe)__**\n ", edit, time.time()))            # Retrieve user's custom renaming preference if set, default to '@devggn' otherwise
+            file = await userbot.download_media(msg, progress=progress_for_pyrogram, progress_args=(client, "**__Unrestricting__: __[Tapori](https://t.me/taporibot_bot)__**\n ", edit, time.time()))            # Retrieve user's custom renaming preference if set, default to '@devggn' otherwise
             if not file:
               await client.send_message(sender, "Failed to download the media.")
               return None
@@ -478,7 +478,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, file_n):
             await upm.delete()
             return None
         except (ChannelBanned, ChannelInvalid, ChannelPrivate, ChatIdInvalid, ChatInvalid):
-            await client.edit_message_text(sender, edit_id, "Bot is not in that channel/group \nsend the invite or add session vioa command /addsession link so that bot can join the channel\n\nTo generate session you can use our official bot - @stringsessionAK47bot")
+            await client.edit_message_text(sender, edit_id, "Bot is not in that channel/group \nsend the invite or add session vioa command /addsession link so that bot can join the channel\n\nTo generate session you can use our official bot - @StringFatherBot_Bot")
             return None
     else:
         edit = await client.edit_message_text(sender, edit_id, "Cloning.")
@@ -496,7 +496,7 @@ async def get_bulk_msg(userbot, client, sender, msg_link, i):
 
 # ------------------------ Button Mode Editz FOR SETTINGS ----------------------------
 
-SESSION_CHANNEL = -1002248639481
+SESSION_CHANNEL = -1002226781947
 # MongoDB database name and collection name
 DB_NAME = "smart_users"
 COLLECTION_NAME = "super_user"
@@ -635,7 +635,7 @@ async def settings_command(event):
         [Button.inline("Remove Words", b'delete')],
         [Button.inline("Login", b'addsession'), Button.inline("Logout", b'logout')],
         [Button.inline("Set Thumbnail", b'setthumb'), Button.inline("Remove Thumbnail", b'remthumb')],
-        [Button.url("Report Errors", "https://t.me/Rishavdevkrr")]
+        [Button.url("Report Errors", "https://t.me/taporibot_bot")]
     ]
     
     await gf.send_file(
