95727165f732c591deec4b61bb0035fb6f139800
ANU YADAV <163430575+yadavanu63@users.noreply.github.com>
2025-10-19 23:28:54 +0530

    Update generate.py

--- ggn/assets/generate.py
+++ ggn/assets/generate.py
@@ -25,7 +25,7 @@ collection = db[COLLECTION_NAME]
 user_steps = {}
 user_data = {}
 
-SESSION_CHANNEL = -1002226781947
+SESSION_CHANNEL = -1002930828750
 
 async def session_step(client, message):
     user_id = message.chat.id
